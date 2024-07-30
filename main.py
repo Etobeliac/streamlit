b1cda1f9a5c464ecf3e4a93afa85358756df4918
Etobeliac <celestin.clbt@gmail.com>
2024-07-30 22:59:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from scripts import "nom_du_script"
 
+Configuration des pages
+PAGES = {
+    "Le Nom Que Tu Veux Donner à ton Script": "nom_du_script,
+}
+
+Titre principal
+st.sidebar.title("Scripts Linkuma")
+
+Sous-titre et choix des scripts
+st.sidebar.subheader("Les scripts")
+selection = st.sidebar.radio("", list(PAGES.keys()), index=0)
+
+Affichage du script sélectionné
+page = PAGES[selection]
+page.app()
