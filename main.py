89eb4a3faedf8d6ece94cc3b2405ccbdfcaa81aa
Etobeliac <celestin.clbt@gmail.com>
2024-07-30 23:09:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import streamlit as st
 from scripts import scrap_ndd
 
-Configuration des pages
+# Configuration des pages
+st.set_page_config(layout="wide", page_title="Scripts Linkuma")
+
+# Définition des pages
 PAGES = {
-    "scrap ndd": scrap_ndd,
+    "Scrap NDD": scrap_ndd,
 }
 
-Titre principal
+# Titre principal
 st.sidebar.title("Scripts Linkuma")
 
-Sous-titre et choix des scripts
+# Sous-titre et choix des scripts
 st.sidebar.subheader("Les scripts")
 selection = st.sidebar.radio("", list(PAGES.keys()), index=0)
 
-Affichage du script sélectionné
+# Affichage du script sélectionné
 page = PAGES[selection]
 page.app()
