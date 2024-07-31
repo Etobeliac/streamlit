67bddfb669bef9154b2eddfb8c270530ae20dce0
Etobeliac <celestin.clbt@gmail.com>
2024-07-31 10:11:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import pandas as pd
 import re
+import io
 
-# Charger les thématiques depuis le fichier Excel
-def load_thematiques(file_path):
-    df_template = pd.read_excel(file_path)
-    thematique_dict = {}
-    current_thematique = None
-    for index, row in df_template.iterrows():
-        if pd.notna(row['THEMATIQUE FR']):
-            current_thematique = row['THEMATIQUE FR']
-            thematique_dict[current_thematique] = []
-        if pd.notna(row['MENU FR']) and current_thematique:
-            thematique_dict[current_thematique].append(row['MENU FR'])
-    return thematique_dict
+# Dictionnaire des thématiques et mots-clés
+thematique_dict = {
+    'ANIMAUX': ['animal', 'pet', 'zoo', 'farm', 'deer', 'chiens', 'chats', 'animaux'],
+    'CUISINE': ['cook', 'recipe', 'cuisine', 'food', 'bon plan', 'equipement', 'minceur', 'produit', 'restaurant'],
+    'ENTREPRISE': ['business', 'enterprise', 'company', 'corporate', 'formation', 'juridique', 'management', 'marketing', 'services'],
+    'FINANCE / IMMOBILIER': ['finance', 'realestate', 'investment', 'property', 'assurance', 'banque', 'credits', 'immobilier'],
+    'INFORMATIQUE': ['tech', 'computer', 'software', 'IT', 'high tech', 'internet', 'jeux-video', 'marketing', 'materiel', 'smartphones'],
+    'MAISON': ['home', 'house', 'garden', 'interior', 'deco', 'demenagement', 'equipement', 'immo', 'jardin', 'maison', 'piscine', 'travaux'],
+    'MODE / FEMME': ['fashion', 'beauty', 'cosmetics', 'woman', 'beaute', 'bien-etre', 'lifestyle', 'mode', 'shopping'],
+    'SANTE': ['health', 'fitness', 'wellness', 'medical', 'hospital', 'grossesse', 'maladie', 'minceur', 'professionnels', 'sante', 'seniors'],
+    'SPORT': ['sport', 'fitness', 'football', 'soccer', 'basketball', 'tennis', 'autre sport', 'basket', 'combat', 'foot', 'musculation', 'velo'],
+    'TOURISME': ['travel', 'tourism', 'holiday', 'vacation', 'bon plan', 'camping', 'croisiere', 'location', 'tourisme', 'vacance', 'voyage'],
+    'VEHICULE': ['vehicle', 'car', 'auto', 'bike', 'bicycle', 'moto', 'produits', 'securite', 'voiture']
+}
 
 # Classifier un domaine par thématique
 def classify_domain(domain, categories):
@@ -26,54 +29,51 @@ def classify_domain(domain, categories):
 # Fonction principale pour le script Streamlit
 def main():
     st.title("Classification des noms de domaine par thématique")
-
-    # Charger les thématiques
-    thematique_file_path = 'TEMPLATE THEMATIQUES.xlsx'
-    thematiques = load_thematiques(thematique_file_path)
     
-    # Ajustements et exclusion de certaines thématiques
-    sensitive_keywords = ['religion', 'sex', 'voyance', 'escort']
-    sensitive_regex = re.compile(r'\b(?:%s)\b' % '|'.join(map(re.escape, sensitive_keywords)), re.IGNORECASE)
-    year_regex = re.compile(r'\b(19[0-9]{2}|20[0-9]{2})\b')
-
     # Saisie des noms de domaine
     domaines_input = st.text_area("Entrez les noms de domaine (un par ligne)")
-
+    
     if st.button("Analyser"):
         if domaines_input:
             domaines = [domain.strip() for domain in domaines_input.split('\n') if domain.strip()]
-
+            
             # Classifier les domaines
             classified_domains = []
             excluded_domains = []
+            sensitive_keywords = ['religion', 'sex', 'voyance', 'escort']
+            sensitive_regex = re.compile(r'\b(?:%s)\b' % '|'.join(map(re.escape, sensitive_keywords)), re.IGNORECASE)
+            year_regex = re.compile(r'\b(19[0-9]{2}|20[0-9]{2})\b')
+            
             for domain in domaines:
                 if sensitive_regex.search(domain) or year_regex.search(domain):
                     excluded_domains.append(domain)
                 else:
-                    category = classify_domain(domain, thematiques)
+                    category = classify_domain(domain, thematique_dict)
                     classified_domains.append((domain, category))
             
             # Créer le DataFrame pour les résultats
-            df = pd.DataFrame(classified_domains, columns=['Domain', 'Category'])
+            df_classified = pd.DataFrame(classified_domains, columns=['Domain', 'Category'])
             df_excluded = pd.DataFrame(excluded_domains, columns=['Domain'])
             df_excluded['Category'] = 'EXCLU'
             
             # Afficher la prévisualisation des résultats
             st.subheader("Prévisualisation des résultats")
-            st.write(df)
+            st.write(df_classified)
             st.write(df_excluded)
             
             # Ajouter une option pour télécharger les résultats
             def convert_df_to_excel(df1, df2):
-                output = pd.ExcelWriter('domaines_classes_resultats.xlsx', engine='xlsxwriter')
-                df1.to_excel(output, index=False, sheet_name='Classified')
-                df2.to_excel(output, index=False, sheet_name='Excluded')
-                output.save()
+                output = io.BytesIO()
+                writer = pd.ExcelWriter(output, engine='xlsxwriter')
+                df1.to_excel(writer, index=False, sheet_name='Classified')
+                df2.to_excel(writer, index=False, sheet_name='Excluded')
+                writer.save()
+                output.seek(0)
                 return output
 
             st.download_button(
                 label="Télécharger les résultats en Excel",
-                data=convert_df_to_excel(df, df_excluded).getvalue(),
+                data=convert_df_to_excel(df_classified, df_excluded),
                 file_name="domaines_classes_resultats.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
