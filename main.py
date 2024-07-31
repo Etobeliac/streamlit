719c19b61e38c309b28172c02a33c0c0b55aa600
Etobeliac <celestin.clbt@gmail.com>
2024-07-31 14:10:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ thematique_dict = {
     'CUISINE': ['cook', 'recipe', 'cuisine', 'food', 'bon plan', 'equipement', 'minceur', 'produit', 'restaurant'],
     'ENTREPRISE': ['business', 'enterprise', 'company', 'corporate', 'formation', 'juridique', 'management', 'marketing', 'services'],
     'FINANCE / IMMOBILIER': ['finance', 'realestate', 'investment', 'property', 'assurance', 'banque', 'credits', 'immobilier'],
-    'INFORMATIQUE': ['tech', 'computer', 'software', 'IT', 'high tech', 'internet', 'jeux-video', 'marketing', 'materiel', 'smartphones'],
+    'INFORMATIQUE': ['tech', 'computer', 'software', 'IT', 'high tech', 'internet', 'jeux-video', 'marketing', 'materiel', 'smartphones', 'file'],
     'MAISON': ['home', 'house', 'garden', 'interior', 'deco', 'demenagement', 'equipement', 'immo', 'jardin', 'maison', 'piscine', 'travaux'],
     'MODE / FEMME': ['fashion', 'beauty', 'cosmetics', 'woman', 'beaute', 'bien-etre', 'lifestyle', 'mode', 'shopping'],
     'SANTE': ['health', 'fitness', 'wellness', 'medical', 'hospital', 'grossesse', 'maladie', 'minceur', 'professionnels', 'sante', 'seniors'],
     'SPORT': ['sport', 'fitness', 'football', 'soccer', 'basketball', 'tennis', 'autre sport', 'basket', 'combat', 'foot', 'musculation', 'velo'],
-    'TOURISME': ['travel', 'tourism', 'holiday', 'vacation', 'bon plan', 'camping', 'croisiere', 'location', 'tourisme', 'vacance', 'voyage'],
+    'TOURISME': ['travel', 'tourism', 'holiday', 'vacation', 'bon plan', 'camping', 'croisiere', 'location', 'tourisme', 'vacance', 'voyage', 'chamber'],
     'VEHICULE': ['vehicle', 'car', 'auto', 'bike', 'bicycle', 'moto', 'produits', 'securite', 'voiture']
 }
 
@@ -69,43 +69,9 @@ def main():
             excluded_and_non_utilise_domains = []
             
             for domain in domaines:
-                if excluded_regex.search(domain) or year_regex.search(domain):
+                if excluded_regex.search(domain) or year_regex.search(domain) or is_name(domain):
                     excluded_and_non_utilise_domains.append((domain, 'EXCLU', determine_language(domain)))
                 else:
                     category = classify_domain(domain, thematique_dict)
                     language = determine_language(domain)
-                    if category == 'NON UTILISÉ':
-                        excluded_and_non_utilise_domains.append((domain, category, language))
-                    else:
-                        classified_domains.append((domain, category, language))
-            
-            # Créer le DataFrame pour les résultats
-            df_classified = pd.DataFrame(classified_domains, columns=['Domain', 'Category', 'Language'])
-            df_excluded_and_non_utilise = pd.DataFrame(excluded_and_non_utilise_domains, columns=['Domain', 'Category', 'Language'])
-            
-            # Afficher la prévisualisation des résultats
-            st.subheader("Prévisualisation des résultats")
-            st.write(df_classified)
-            st.write(df_excluded_and_non_utilise)
-            
-            # Ajouter une option pour télécharger les résultats
-            def convert_df_to_excel(df1, df2):
-                output = io.BytesIO()
-                writer = pd.ExcelWriter(output, engine='xlsxwriter')
-                df1.to_excel(writer, index=False, sheet_name='Classified')
-                df2.to_excel(writer, index=False, sheet_name='Excluded and Non Utilisé')
-                writer.close()
-                output.seek(0)
-                return output
-
-            st.download_button(
-                label="Télécharger les résultats en Excel",
-                data=convert_df_to_excel(df_classified, df_excluded_and_non_utilise),
-                file_name="domaines_classes_resultats.xlsx",
-                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-            )
-        else:
-            st.warning("Veuillez entrer au moins un nom de domaine.")
-
-if __name__ == "__main__":
-    main()
+                    if category == 'NON
