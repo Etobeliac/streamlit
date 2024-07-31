d0c8e1c5860aee9e1bf7d74533fa27d0d9b2d675
Etobeliac <celestin.clbt@gmail.com>
2024-07-31 09:55:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-import streamlit as st
-import os
-
-# Chemin du dossier contenant les scripts
-SCRIPTS_FOLDER = 'scripts'
-
-# Titre de l'application
-st.title("Scripts Viewer")
-
-# Lister les fichiers dans le dossier des scripts et filtrer uniquement les fichiers
-scripts = [f for f in os.listdir(SCRIPTS_FOLDER) if os.path.isfile(os.path.join(SCRIPTS_FOLDER, f))]
-
-# Afficher la liste des scripts à gauche
-selected_script = st.sidebar.selectbox("Select a script", scripts)
-
-# Lire et afficher le contenu du script sélectionné
-if selected_script:
-    script_path = os.path.join(SCRIPTS_FOLDER, selected_script)
-    with open(script_path, 'r') as file:
-        script_content = file.read()
-    st.subheader(f"Content of {selected_script}")
-    st.code(script_content, language='python')
-else:
-    st.subheader("Select a script to view its content")
-
 import os
 
 # Exécuter le script classifier.py avec Streamlit
 os.system('streamlit run scripts/classifier.py')
-
-
